6d4895d5debda565fe8efb4be73b2934c48a87c7
Bogdan <bogdantisenko06@gmail.com>
2024-02-29 23:21:34 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import { GlobalStyle } from './components/GlobalStyled.js';
 

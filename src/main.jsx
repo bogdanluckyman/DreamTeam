748bf927a5822f1f52dc04157ade66de963a8a02
Bogdan <bogdantisenko06@gmail.com>
2024-02-29 23:26:39 +0200

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { GlobalStyle } from './components/GlobalStyled.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/bogdanluckyman/DreamTeam">
+    <BrowserRouter basename="/">
       <App />
       <GlobalStyle />
     </BrowserRouter>

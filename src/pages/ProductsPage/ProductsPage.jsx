8e9197aad47585df6892a570c96b9bc727add0b4
AnastasiiaKalashnikova <nastasyakalashnikova@icloud.com>
2024-03-04 21:18:11 +0100

    .

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,24 +1,32 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { TitlePage } from '../../components/TitlePage/TitlePage';
 import { useEffect } from 'react';
-import { ProductsFilter } from '../../components/ProductsFilters/ProductsFilters';
+//import { ProductsFilter } from '../../components/ProductsFilters/ProductsFilters';
 import { ProductsList } from '../../components/ProductsList/ProductsList';
+import { fetchProducts } from '../../redux/products/operations';
+import { ProductsFilters } from '../../components/ProductsFilters/ProductsFilters';
 
 export default function ProductsPage() {
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
+  //  const isLoading = useSelector(selectIsLoading);
+  //  const error = useSelector(selectError);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(fetchProducts());
   }, [dispatch]);
 
   return (
     <>
       <TitlePage title="Products" />
-      <ProductsFilter />
-      {isLoading && !error && <h1>зробити лодер</h1>}
-      {error && <p>Sorry! {error}. Try again!</p>}
+      <ProductsFilters />
       <ProductsList />
     </>
   );
 }
+// повне версія
+//  <>
+//    <TitlePage title="Products" />
+// <ProductsFilter />
+// {isLoading && !error && <h1>зробити лодер</h1>}
+// {error && <p>Sorry! {error}. Try again!</p>}
+//<ProductsList />;
+// </>

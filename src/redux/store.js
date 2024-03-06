1bcd54532b281802bdd37345e237bb8f952100a2
NadiiaChernokur <chiernokur5@gmail.com>
2024-03-06 16:00:13 +0100

    bag

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,4 @@
 // import { combineReducers, configureStore } from '@reduxjs/toolkit';
-// import { diaryReducer } from './toGet';
-
-// const rootReducer = combineReducers({
-//   diary: diaryReducer,
-// });
-
-// export const store = configureStore({
-//   reducer: rootReducer,
-// });
-
-// import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 // const rootReducer = combineReducers({});
 
--- src/redux/toGet.js
+++ src/redux/toGet.js
@@ -89,3 +89,22 @@ export const diarySlice = createSlice({
 });
 
 export const diaryReducer = diarySlice.reducer;
+
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import { diaryReducer } from './toGet';
+
+// const rootReducer = combineReducers({
+//   diary: diaryReducer,
+// });
+
+// export const store = configureStore({
+//   reducer: rootReducer,
+// });
+
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+// const rootReducer = combineReducers({});
+
+// export const store = configureStore({
+//   reducer: rootReducer,
+// });

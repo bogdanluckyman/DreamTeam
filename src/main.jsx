47fc997ba2cbf852cbfbc72dd71006b5374db11c
Bogdan <bogdantisenko06@gmail.com>
2024-03-06 17:28:35 +0200

    persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,18 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { GlobalStyle } from './styles/GlobalStyled.js';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'react-datepicker/dist/react-datepicker.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/DreamTeam">
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
       <GlobalStyle />
     </BrowserRouter>

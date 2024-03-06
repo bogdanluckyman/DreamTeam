099b8275bb49164569620a07dd35740a52c6a18f
NadiiaChernokur <chiernokur5@gmail.com>
2024-03-06 15:26:50 +0100

    comm

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,47 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
 // import { diaryReducer } from './toGet';
 
-const rootReducer = combineReducers({
-  // diary: diaryReducer,
-});
+// const rootReducer = combineReducers({
+//   diary: diaryReducer,
+// });
+
+// export const store = configureStore({
+//   reducer: rootReducer,
+// });
+
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth/slice';
+// import { productsReducer } from './products/sliceProducts';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+
+    // products: productsReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);

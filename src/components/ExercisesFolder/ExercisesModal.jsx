92ef938de804bc5bd637d76d6a09e7f296a6bdf5
NadiiaChernokur <chiernokur5@gmail.com>
2024-03-07 15:52:04 +0100

    count

--- src/components/ExercisesFolder/ExercisesModal.jsx
+++ src/components/ExercisesFolder/ExercisesModal.jsx
@@ -28,8 +28,8 @@ const ExercisesModal = ({ onClose }) => {
   const [restart, setReStart] = useState(false);
   const [closeModal, setcloseModal] = useState(false);
   const [counter, setCounter] = useState(0);
-  // const [intervalId, setIntervalId] = useState(null);
-  const [seconds, setSeconds] = useState(0);
+  const [seconds, setSeconds] = useState(1);
+  const [overallResult, setOverallResult] = useState(3);
   /////////////////////////////////////////////////////////////
   const exercise = {
     _id: '64f2458d6f67bc34bae4f86d',
@@ -41,12 +41,13 @@ const ExercisesModal = ({ onClose }) => {
     burnedCalories: 164,
     time: 3,
   };
+
+  console.log(overallResult);
   const exercCal = exercise.burnedCalories;
 
   const title = exercise.name.replace(/(^\w|\.\s\w)/g, (char) =>
     char.toUpperCase()
   );
-
   const target = exercise.target.replace(/(^\w|\.\s\w)/g, (char) =>
     char.toUpperCase()
   );
@@ -62,7 +63,17 @@ const ExercisesModal = ({ onClose }) => {
       onClose();
     }
   });
-
+  // const Formula = (time) => {
+  //   const [minutes, secound] = time.split(':').map(Number);
+  //   const totalSeconds = minutes * 60 + secound;
+  //   const res = 180 - totalSeconds;
+  //   const formattedTime = `${Math.floor(res / 10)}:${res % 10 < 10 ? '0' : ''}${
+  //     res % 10
+  //   }`;
+  //   console.log(formattedTime);
+  //   console.log(overallResult);
+  // };
+  console.log(counter);
   useEffect(() => {
     const calculateCaloriesBurned = (timeInMinutes) => {
       const caloriesPer3Min = exercCal;
@@ -75,27 +86,36 @@ const ExercisesModal = ({ onClose }) => {
       }, 1000);
     }
     const minutes = seconds / 60;
+    console.log(minutes);
     const calculatedCalories = calculateCaloriesBurned(minutes);
-    setCounter(calculatedCalories.toFixed(2));
+    console.log(calculatedCalories);
+    console.log(counter);
+    setCounter(calculatedCalories.toFixed(15));
     return () => clearInterval(timer);
   }, [counter, exercCal, isPaused, seconds]);
 
   useEffect(() => {
     if (time === '' && restart === true) {
+      setOverallResult((prev) => prev + 3);
       setStart(false);
       setReStart(false);
       setIsPaused(true);
+
       return;
     }
 
     return;
-    //  () => clearInterval(intervalId);
   }, [restart, start, time]);
 
   const toCloseWindiw = () => {
-    const timeRes = 3 - time;
-
-    console.log(timeRes);
+    // const newObjekt = {
+    //   date: { date },
+    //   products: {
+    //     productID: { id },
+    //     amount: value,
+    //     calories: getCalories,
+    //   },
+    // };
     setcloseModal(true);
   };
 
@@ -104,9 +124,11 @@ const ExercisesModal = ({ onClose }) => {
     const seconds = remainingTime % 60;
     const res = `${minutes}:${seconds}`;
 
+    console.log('7777777777777');
     if (time === '0:0') {
       setTime('');
       setReStart(true);
+
       return;
     }
 
@@ -171,7 +193,7 @@ const ExercisesModal = ({ onClose }) => {
                   )}
                 </PlayPause>
                 <BurnedCalories>
-                  Burned calories: <TimeSpan>{counter}</TimeSpan>
+                  Burned calories: <TimeSpan>{Math.floor(counter)}</TimeSpan>
                 </BurnedCalories>
               </TimerDiv>
               <DataContainerDiv>
@@ -205,6 +227,7 @@ const ExercisesModal = ({ onClose }) => {
         <ExercisesWellDoneModal
           onClose={onClose}
           time={time}
+          timeres={overallResult}
           calori={counter}
           background={handleBackgroundClick}
         />
--- src/components/ExercisesFolder/ExercisesWellDoneModal.jsx
+++ src/components/ExercisesFolder/ExercisesWellDoneModal.jsx
@@ -12,14 +12,50 @@ import {
   InnerExercisesWellDoneContainer,
 } from './ExercisesWellDoneModal.styles';
 
-const ExercisesWellDoneModal = ({ onClose, time, calori, background }) => {
+const ExercisesWellDoneModal = ({
+  onClose,
+  time,
+  calori,
+  background,
+  timeres,
+}) => {
+  console.log(timeres);
+  console.log(time);
+  const number = timeres - 3;
+  const formattedTimer = `${number}:00`;
+  console.log(formattedTimer);
+
   const [minutes, secound] = time.split(':').map(Number);
   const totalSeconds = minutes * 60 + secound;
   const res = 180 - totalSeconds;
-  const formattedTime = `${Math.floor(res / 10)}:${res % 10 < 10 ? '0' : ''}${
-    res % 10
-  }`;
 
+  const formatMinutes = Math.floor(res / 60);
+  const formatSeconds = res % 60;
+
+  const formattedTime = `${formatMinutes}:${
+    formatSeconds < 10 ? '0' : ''
+  }${formatSeconds}`;
+  console.log(formattedTime); // Виведе "0:10"
+
+  // const formattedTime = `${Math.floor(res / 10)}:${res % 10 < 10 ? '0' : ''}${
+  //   res % 10
+  // }`;
+  // console.log(formattedTime);
+
+  const time1 = formattedTimer;
+  const time2 = formattedTime;
+
+  const [min1, sec1] = time1.split(':').map(Number);
+  const [min2, sec2] = time2.split(':').map(Number);
+
+  const totalMinutes = min1 + min2;
+  const totalSecond = sec1 + sec2;
+
+  const formattedMinutes = totalMinutes.toString().padStart(2, '0');
+  const formattedSeconds = totalSecond.toString().padStart(2, '0');
+  const totalFormattedTime = `${formattedMinutes}:${formattedSeconds}`;
+
+  console.log(totalFormattedTime);
   return (
     <ExercisesWellDoneBackground onClick={background}>
       <ExercisesWellDoneContainer>
@@ -33,11 +69,11 @@ const ExercisesWellDoneModal = ({ onClose, time, calori, background }) => {
           <ExercisesText>Well done</ExercisesText>
           <ExercisesParagraph>
             Your time:
-            <ExercisesSpan>{formattedTime}</ExercisesSpan>
+            <ExercisesSpan>{totalFormattedTime}</ExercisesSpan>
           </ExercisesParagraph>
           <ExercisesParagraph>
             Burned calories:
-            <ExercisesSpan>{calori}</ExercisesSpan>
+            <ExercisesSpan>{Math.floor(calori)}</ExercisesSpan>
           </ExercisesParagraph>
           <ExercisesNextProduct>Next product</ExercisesNextProduct>
           <ExercisesDiaryButton>

4bf62f0abe1d0e1f95667d5842678bda456fcb48
Yurii <gapirov21@gmail.com>
2024-03-11 22:57:19 +0200

    Update Loader.styled.js

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -2,4 +2,9 @@ import styled from 'styled-components';
 
 export const LoaderCont = styled.div`
   text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  margin: 0;
 `;

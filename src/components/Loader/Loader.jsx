768710fdfda96822fcc25b11daed02b8cccdab3f
Bogdan <bogdantisenko06@gmail.com>
2024-03-11 20:47:09 +0200

    miniFix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,6 @@
 import { LoaderCont } from './Loader.styled';
 import { Heart } from 'react-spinners-css';
 
-export const Loader = ({ isLoading }) => {
-  return <LoaderCont>{isLoading && <Heart color={'#e6533c'} />}</LoaderCont>;
-}
+export const Loader = () => {
+  return <LoaderCont>{<Heart color={'#e6533c'} />}</LoaderCont>;
+};

5645fdb247354fe9f2a808ad62a058bf48f585db
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2024-03-11 13:44:33 +0200

    headerhotfix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { HeaderContainer, Navigation, StyledLink, HeaderLogo, LogoSvg, BurgerSvg, BurgerMenu, SettingsMenu, SettingsSvg, Avatar, SettingImg } from './Header.styled';
+import { HeaderContainer, Navigation, StyledLink, HeaderLogo, LogoSvg, BurgerSvg, BurgerMenu, SettingsMenu, SettingsSvg, Avatar, SettingImg, LogoButton } from './Header.styled';
 import sprite from '../../img/sprite.svg';
 import ModalMenu from '../ProductsItem/ModalMenu';
 import { useMediaQuery } from '@react-hook/media-query'; 
@@ -26,11 +26,11 @@ export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderLogo>
-        <a href='/'>
+        <LogoButton onClick={() => navigate('/diary')}>
           <LogoSvg className="icon">
             <use xlinkHref={`${sprite}#${isWideScreen ? 'icon-logo' : 'icon-logo-mobile'}`} />
           </LogoSvg>
-        </a>
+        </LogoButton>
       </HeaderLogo>
       <Navigation>
         

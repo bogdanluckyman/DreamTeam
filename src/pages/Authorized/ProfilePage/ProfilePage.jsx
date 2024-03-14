6a16fe759078f58f62791f712eef76ae48e29377
Angelica <lisoveckaya1997@gmail.com>
2024-03-14 23:40:46 +0200

    loader

--- src/pages/Authorized/ProfilePage/ProfilePage.jsx
+++ src/pages/Authorized/ProfilePage/ProfilePage.jsx
@@ -1,14 +1,21 @@
 
+import { useSelector } from 'react-redux';
 import { TitlePage } from '../../../components/TitlePage/TitlePage';
 import UserCard from '../../../components/UserCard/UserCard';
 import UserForm from '../../../components/UserForm/UserForm';
+
 import { Container, WrapperContent, WrapperForm } from './ProfilePage.styled';
+import { selectIsRefreshing} from '../../../redux/auth/selectors';
+import { Loader } from '../../../components/Loader/Loader';
 
 
 const ProfilePage = () => {
+  const isRefreshing = useSelector(selectIsRefreshing);
+  
+  
 
 
-  return (
+  return isRefreshing ? (<Loader/>) : (
     <Container>
       <TitlePage title="Profile Settings" />
       <WrapperContent>
